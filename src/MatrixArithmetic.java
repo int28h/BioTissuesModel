6e6442d938c3ee4acc615d26651654ed072436e9
Elena Listratova <int28h@protonmail.com>
2018-01-17 18:24:34 +0400

    Matrix full

--- src/MatrixArithmetic.java
+++ src/MatrixArithmetic.java
@@ -1,4 +1,36 @@
+//здесь арифметические операции над матрицами
 
 public class MatrixArithmetic {
+	public static double[][] multiplicationMatrixNumber(double[][] m, double n) {
+		double[][] result = new double[4][4];		
+		for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; j++) {
+            	result[i][j] = m[i][j] * n;
+            }
+        }		
+		return result;
+	}
 	
+	public static double[][] multiplicationMatrixMatrix(double[][] m1, double [][]m2) {
+		double[][] result = new double[4][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				result[i][j] = 0;
+				for (int k = 0; k < 4; k++) {
+					result[i][j] += m1[i][k] * m2[k][j];
+				}
+			}
+	    }
+		return result;
+	}
+	
+	public static double[][] additionMatrixMatrix(double[][] m1, double [][]m2) {
+		double[][] result = new double[4][4];
+		for (int i = 0; i < 4; ++i){
+            for (int j = 0; j < 4; j++){
+                    result[i][j] = m1[i][j] + m2[i][j];
+                }
+        }
+		return result;
+	}
 }
